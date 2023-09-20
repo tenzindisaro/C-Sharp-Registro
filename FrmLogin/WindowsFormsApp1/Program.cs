6cdd351da9891469c3026adb7a7cb7f01f64d0b3
Angelo <henriqueangelo0611@gmail.com>
2023-09-20 20:56:44 -0300

    Começo da validação de usuário e teste da classe List<>

--- FrmLogin/WindowsFormsApp1/Program.cs
+++ FrmLogin/WindowsFormsApp1/Program.cs
@@ -17,6 +17,7 @@ namespace WindowsFormsApp1
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new FrmLogin());
+            Application.Run(new Form2());
         }
     }
 }
