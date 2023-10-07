d7803546b5a331727aed0fe52ba7439c847bd8c6
Angelo <henriqueangelo0611@gmail.com>
2023-10-07 14:55:51 -0300

    CadastroUser Form

--- FrmLogin/WindowsFormsApp1/CadastroUser/Form4TelaCadastrosUsers.Designer.cs
+++ FrmLogin/WindowsFormsApp1/CadastroUser/Form4TelaCadastrosUsers.Designer.cs
@@ -110,6 +110,7 @@
             this.textBox4.Name = "textBox4";
             this.textBox4.Size = new System.Drawing.Size(100, 20);
             this.textBox4.TabIndex = 56;
+            //this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
             // 
             // label4
             // 
